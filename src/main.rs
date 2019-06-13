2d5063b6d6108886ebcf49fc5220f44599c5be14
Joel Palmer <joelpalmer@gmail.com>
2019-06-13 09:29:48 -0500

    start plugging in the clap args

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,17 @@ use clap::{App, Arg, SubCommand};
 mod game;
 
 fn main() {
-    println!("The Dodgers won!");
+    let matches = App::new("MLB Scores & Stats")
+        .version("1.0")
+        .author("Joel Palmer <joel@joel.systems>")
+        .about("Only gets Dodgers scores for now")
+        .arg(
+            Arg::with_name("TEAM")
+                .help("Sets the team to look up")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    println!("Team: {}", matches.value_of("TEAM").unwrap());
 }
