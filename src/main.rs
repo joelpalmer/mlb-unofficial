b143c55d0755728884520cd20af4108d6bf741f6
Joel Palmer <joelpalmer@gmail.com>
2019-06-13 08:49:51 -0500

    adding clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
 mod game;
 
 fn main() {
-    // Initial Requirements:
-    //  todo: Take in arguments for `team name` & `date of game` (default today) -> score & maybe opponent...
-    //  Data Source: https://statsapi.mlb.com/api/v1/schedule?sportId=1,51&date=2019-06-10
-    //  todo: add steps like json parsing etc...
-
     println!("The Dodgers won!");
 }
